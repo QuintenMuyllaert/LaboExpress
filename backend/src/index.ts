7a4006cf265d5586003dae139122c4f33925615a
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-09-26 16:09:24 +0200

    ✨ Made the express api routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+
+import { logger } from "./middleware/logger";
+import radioRepository from "./repository/radioRepository";
+
 dotenv.config();
 
 const port: number = parseInt(process.env.PORT || "80");
@@ -8,9 +12,48 @@ const host: string = process.env.HOST || "0.0.0.0";
 const app = express();
 
 app.use(express.json());
+app.use(logger());
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello World!");
+app.get("/radio", (req: Request, res: Response) => {
+	res.send(radioRepository.getAll());
+});
+
+app.post("/radiostation", (req: Request, res: Response) => {
+	const { naam } = req.body;
+	if (!naam) {
+		res.status(400).send("Geen naam opgegeven");
+		return;
+	}
+
+	const id = radioRepository.add({ naam });
+	res.status(201).send({ id });
+});
+
+app.get("/radiostation", (req: Request, res: Response) => {
+	const id = req?.query?.id as unknown as string;
+	if (!id) {
+		res.status(400).send("Missing id");
+		return;
+	}
+
+	const radiostation = radioRepository.getById(parseInt(id));
+	if (!radiostation) {
+		res.status(404).send("Not found");
+		return;
+	}
+
+	res.send(radiostation);
+});
+
+app.delete("/radiostation", (req: Request, res: Response) => {
+	const id = req?.query?.id as unknown as string;
+	if (!id) {
+		res.status(400).send("Missing id");
+		return;
+	}
+
+	radioRepository.delete(parseInt(id));
+	res.status(204).send();
 });
 
 app.listen(port, host, () => {
